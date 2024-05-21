4901a53ded02216a3cf9f1c7fff4e1c87b7b8075
nekoLin <krusiawanbudi1@gmail.com>
2024-05-21 14:37:32 +0800

    Add custom alert

--- index.php
+++ index.php
@@ -6,37 +6,53 @@
     <link rel="stylesheet" href="style.css">
     <title></title>
     <?php
-        require_once "App/Email.php";
-        if(isset($_POST['formEmail'])) {
-            $Email->sendEmail($_POST);
-        }
+        // require_once "App/Email.php";
+        // if(isset($_POST['formEmail'])) {
+        //     $Email->sendEmail($_POST);
+        // }
     ?>
 </head>
 <body>
     <div class="container">
         <section class="left">
-            <form action="" method="post" class="form">
+            <form action="" method="post" class="form" id="formEmail">
                 <div class="input-group">
                     <label for="email">Email</label>
                     <input type="email" name="email" id="">
                 </div>
-                <!-- <div class="input-group">
-                    <label for="subject">Subject</label>
-                    <input type="text" name="subject" id="">
-                </div> -->
+                <div class="input-group">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" id="">
+                </div>
                 <div class="input-group">
                     <label for="body">Body</label>
-                    <!-- <input type="text" name="body" id=""> -->
                     <textarea name="body" id="body-email" rows="5"></textarea>
                 </div>
                 <div class="form-submit">
-                    <input type="submit" name="formEmail" id="">
+                    <input type="submit" name="formEmail" id="submitEmail">
                 </div>
             </form>
         </section>
         <section class="right">
-            <p>test</p>
+            <p id="statusForm"></p>
         </section>
     </div>
+    <div id="customAlert" class="">
+        <div class="alertBox">
+            <h1 class="alertText">test</h1>
+            <div class="loading">
+                <div class="loadingio-spinner-ripple-nq4q5u6dq7r">
+                    <div class="ldio-x2uulkbinbj">
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
+            </div>
+            <div class="done">
+                <img src="Assets/done.png" alt="">
+            </div>
+        </div>
+    </div>
 </body>
+<script src="script.js"></script>
 </html>
