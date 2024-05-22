39ed905c914639178039de0b9a3c60f203b889eb
nekoLin <krusiawanbudi1@gmail.com>
2024-05-23 02:32:02 +0800

    Update form UI

--- index.php
+++ index.php
@@ -1,58 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title></title>
-    <?php
-        // require_once "App/Email.php";
-        // if(isset($_POST['formEmail'])) {
-        //     $Email->sendEmail($_POST);
-        // }
-    ?>
-</head>
-<body>
-    <div class="container">
-        <section class="left">
-            <form action="" method="post" class="form" id="formEmail">
-                <div class="input-group">
-                    <label for="email">Email</label>
-                    <input type="email" name="email" id="">
-                </div>
-                <div class="input-group">
-                    <label for="username">Username</label>
-                    <input type="text" name="username" id="">
-                </div>
-                <div class="input-group">
-                    <label for="body">Body</label>
-                    <textarea name="body" id="body-email" rows="5"></textarea>
-                </div>
-                <div class="form-submit">
-                    <input type="submit" name="formEmail" id="submitEmail">
-                </div>
-            </form>
-        </section>
-        <section class="right">
-            <p id="statusForm"></p>
-        </section>
-    </div>
-    <div id="customAlert" class="">
-        <div class="alertBox">
-            <h1 class="alertText">test</h1>
-            <div class="loading">
-                <div class="loadingio-spinner-ripple-nq4q5u6dq7r">
-                    <div class="ldio-x2uulkbinbj">
-                        <div></div>
-                        <div></div>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css" type="text/css">
+        <link rel="stylesheet" href="resp.css">
+        <title>test</title>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossorigin="crossorigin">
+        <link
+            href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap"
+            rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <div class="bg"></div>
+            <section class="left">
+                <main>
+                    <header>
+                        <h1 class="page-title title">get in touch</h1>
+                        <small>feel free to contact us about your concern</small>
+                    </header>
+                    <form action="" method="post" class="form" id="formEmail">
+                        <div class="input-group">
+                            <label for="email">Email</label>
+                            <input type="email" name="email" id="" placeholder="M"></div>
+                        <div class="input-group">
+                            <label for="username">Username</label>
+                            <input type="text" name="username" id="" placeholder="M"></div>
+                        <div class="input-group">
+                            <label for="body">Message</label>
+                            <textarea name="body" id="body-email" rows="5" placeholder="Masdasda"></textarea>
+                        </div>
+                        <div class="form-submit">
+                            <input type="submit" name="formEmail" id="submitEmail" value="Send"> 
+                        </div>
+                    </form>
+                </main>
+            </section>
+            <section class="right">
+                <aside>
+                    <header>
+                        <h5 class="title">more about us!</h5>
+                    </header>
+                    <main class="contact-detail">
+                        <p>exponent@domain.com</p>
+                        <p>+62 203941</p>
+                        <p>based in Denpasar, Bali</p>
+                        <p>17.00 - 01.00</p>
+                    </main>
+                </aside>
+            </section>
+        </div>
+
+        <div id="customAlert" class="">
+            <div class="alertBox">
+                <h1 class="alertText">test</h1>
+                <div class="loading">
+                    <div class="loadingio-spinner-ripple-nq4q5u6dq7r">
+                        <div class="ldio-x2uulkbinbj">
+                            <div></div>
+                            <div></div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="done">
-                <img src="Assets/done.png" alt="">
+                <div class="done">
+                    <img src="Assets/done.png" alt="">
+                </div>
             </div>
         </div>
-    </div>
-</body>
-<script src="script.js"></script>
+    </body>
+    <script src="script.js"></script>
 </html>
